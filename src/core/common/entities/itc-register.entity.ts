8e4d0bcc87479e2ff07d40574a9d8c60abe19b08
HAKrRBOY <hakrboy@gmail.com>
2025-04-18 00:39:52 +0530

    ITC Register schema hasbeen modified

--- src/core/common/entities/itc-register.entity.ts
+++ src/core/common/entities/itc-register.entity.ts
@@ -4,6 +4,7 @@ import {
     CreateDateColumn,
     Entity,
     Index,
+    PrimaryGeneratedColumn,
     UpdateDateColumn
 } from 'typeorm';
 
@@ -11,6 +12,8 @@ import {
 @Index(['supplierGstin', 'invoiceNumber', 'invoiceDate'], { unique: true }) // Enforce uniqueness based on key fields? Adjust if needed.
 export class ItcRegister {
 
+    @PrimaryGeneratedColumn()
+    purtrnId!: number;
 
     @Index()
     @Column({ type: 'nvarchar', length: 15 })
@@ -50,16 +53,19 @@ export class ItcRegister {
 
     @Column({ type: 'nvarchar', length: 10, nullable: true }) // INV, CRN, DBN
     documentType?: string;
-
+ 
+    @Column({ type: 'nvarchar', length: 20, nullable: true }) // B2B, SEZWP, DE etc.
+    invType?: number;
+    
+    @Column({ type: 'varchar',  nullable: true }) // B2B, SEZWP, DE etc.
+    series?: string;
+    
     @Column({ type: 'nvarchar', length: 100, nullable: true })
     book?: string; // Reference to specific accounting book/ledger if needed
 
-    @Column({ type: 'nvarchar', length: 20, nullable: true }) // B2B, SEZWP, DE etc.
-    invType?: string;
-
     // You might add Conum here as well if it's part of the core ITC record
-    @Column({ type: 'nvarchar', length: 50, nullable: true })
-    conum?: string;
+    @Column({ type: 'nvarchar', length: 1, nullable: true })
+    conum?: number;
 
     // You might add a status field linked to reconciliation later
     // @Column({ type: 'nvarchar', length: 30, nullable: true, default: 'PendingReconciliation' })
